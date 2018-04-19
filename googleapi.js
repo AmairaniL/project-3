0edc273ee2df39b6cd658ac6c549397bf5bb6e04
Amairani Lopez <alopez21@hawk.iit.edu>
2018-04-18 23:02:48 -0500

    Edited my JS to add the two map locations

--- googleapi.js
+++ googleapi.js
@@ -4,11 +4,15 @@ var current;
 
 //top radioactive locations on Earth.
 var chicago={lat: 41.8562277, lng: -87.6912785}
+var xalapa={lat: 19.5438, lng: -96.9102};
+var coyolillo={lat: 19.567482, lng: -96.746547};
+
+var locations=["Xalapa", "Coyolillo"];
 
 function myMap() {
 	current={lat: 41.8562277, lng: -87.6912785};
 	map = new google.maps.Map(document.getElementById('map'), {
-		zoom:15,
+		zoom:12,
 	center: chicago});
 	marker = new google.maps.Marker({
 		position:chicago,
@@ -16,3 +20,21 @@ function myMap() {
 	map: map });
 }
 
+function travel(location, my_locations) {
+	map.panTo(location);
+	map.setZoom(15);
+	marker = new google.maps.Marker({
+		position: location,
+			animation:google.maps.Animation.BOUNCE,
+		map: map
+	});
+	if(my_locations==locations[0]){
+		document.getElementById("description").innerHTML="Xalapa, Veracruz"
+	}
+	else if(my_locations==locations[1]){
+		document.getElementById("description").innerHTML=""
+	}
+	else {
+		document.getElementById("description").innerHTML=null;
+	}
+}
